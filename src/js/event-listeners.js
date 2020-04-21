422b042b7b61c887402b14d4fbd38e5b4b6a26b3
M. A. Muhaimin Sakib <sakib@appscode.com>
2020-04-21 18:33:44 +0600

    Add eslint, eslint configs, resize modal window, test new ui
    Signed-off-by: M. A. Muhaimin Sakib <sakib@appscode.com>

--- src/js/event-listeners.js
+++ src/js/event-listeners.js
@@ -1,24 +1,24 @@
-import { css } from "./styles";
+import { css } from './styles';
 
-export default function addEventListeners(btn, acModal, acModalClose) {
-  btn.addEventListener("click", e => {
+export default function (btn, acModal, acModalClose) {
+  btn.addEventListener('click', (e) => {
     e.preventDefault();
 
-    css(acModal, { display: "flex" });
+    css(acModal, { display: 'flex' });
   });
 
-  acModalClose.addEventListener("click", e => {
+  acModalClose.addEventListener('click', (e) => {
     e.preventDefault();
 
-    css(acModal, { display: "none" });
+    css(acModal, { display: 'none' });
   });
 
-  document.addEventListener("keydown", e => {
+  document.addEventListener('keydown', (e) => {
     e.preventDefault();
 
-    const keyCode = e.keyCode;
+    const { keyCode } = e;
     if (keyCode === 27) {
-      css(acModal, { display: "none" });
+      css(acModal, { display: 'none' });
     }
   });
 }
--- src/js/styles.js
+++ src/js/styles.js
@@ -1,5 +1,8 @@
 export function css(el, styles) {
-  for (const property in styles) el.style[property] = styles[property];
+  Object.keys(styles).forEach((styleProp) => {
+    // eslint-disable-next-line no-param-reassign
+    el.style[styleProp] = styles[styleProp];
+  });
 }
 
 export function addStyles(
@@ -8,69 +11,67 @@ export function addStyles(
   acModalBg,
   acModalContent,
   acModalBox,
-  acModalClose
+  acModalClose,
 ) {
-  debugger;
-
   css(btn, {
-    backgroundColor: "#4CAF50" /* Green */,
-    border: "none",
-    color: "white",
-    padding: "15px 32px",
-    textAlign: "center",
-    textDecoration: "none",
-    display: "inline-block",
-    fontSize: "16px"
+    backgroundColor: '#4CAF50' /* Green */,
+    border: 'none',
+    color: 'white',
+    padding: '15px 32px',
+    textAlign: 'center',
+    textDecoration: 'none',
+    display: 'inline-block',
+    fontSize: '16px',
   });
 
   css(acModal, {
-    alignItems: "center",
-    display: "none",
-    flexDirection: "column",
-    justifyContent: "center",
-    overflow: "hidden",
-    position: "fixed",
-    zIndex: "40",
-    width: "100%",
-    height: "100%",
-    top: "0",
-    left: "0"
+    alignItems: 'center',
+    display: 'none',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    overflow: 'hidden',
+    position: 'fixed',
+    zIndex: '40',
+    width: '100%',
+    height: '100%',
+    top: '0',
+    left: '0',
   });
 
   css(acModalBg, {
-    backgroundColor: "rgba(10,10,10,.86)",
-    bottom: "0",
-    left: "0",
-    position: "absolute",
-    right: "0",
-    top: "0"
+    backgroundColor: 'rgba(10,10,10,.86)',
+    bottom: '0',
+    left: '0',
+    position: 'absolute',
+    right: '0',
+    top: '0',
   });
 
   css(acModalContent, {
-    margin: "0 auto",
-    maxHeight: "calc(100vh - 80px)",
-    width: "80%",
-    overflow: "auto",
-    position: "relative"
+    margin: '0 auto',
+    maxHeight: 'calc(100vh - 20px)',
+    width: '80%',
+    overflow: 'auto',
+    position: 'relative',
   });
 
   css(acModalBox, {
-    backgroundColor: "#fff",
-    borderRadius: "6px",
+    backgroundColor: '#fff',
+    borderRadius: '6px',
     boxShadow:
-      "0 0.5em 1em -0.125em rgba(10,10,10,.1), 0 0 0 1px rgba(10,10,10,.02)",
-    color: "#4a4a4a",
-    display: "block"
+      '0 0.5em 1em -0.125em rgba(10,10,10,.1), 0 0 0 1px rgba(10,10,10,.02)',
+    color: '#4a4a4a',
+    display: 'block',
   });
 
   css(acModalClose, {
-    position: "absolute",
-    right: "40px",
-    top: "40px",
-    backgroundColor: "transparent",
-    borderColor: "transparent",
-    color: "#fff",
-    fontSize: "22px",
-    cursor: "pointer"
+    position: 'absolute',
+    right: '40px',
+    top: '40px',
+    backgroundColor: 'transparent',
+    borderColor: 'transparent',
+    color: '#fff',
+    fontSize: '22px',
+    cursor: 'pointer',
   });
 }
