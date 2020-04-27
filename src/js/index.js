e2c56b8e0c1293c86c886d83b52e89accd904175
M. A. Muhaimin Sakib <sakib@appscode.com>
2020-04-27 15:57:24 +0600

    Add bordered button style, add icon to button and add hover effect
    Signed-off-by: M. A. Muhaimin Sakib <sakib@appscode.com>

--- src/js/index.js
+++ src/js/index.js
@@ -4,13 +4,12 @@ import addEventListeners from './event-listeners';
 
 export function initialize(ref) {
   const el = document.querySelector(ref);
-  el.innerHTML = `<button class="ac-bb-modal-button">Deploy with ByteBuilders</button>
-
+  el.innerHTML = `<button class="ac-bb-modal-button"><img class="ac-bb-modal-button-icon" src="https://github.com/appscode/static-assets/raw/master/images/products/bytebuilders/bytebuilders-512x512.png" alt width="40"/>Deploy with ByteBuilders</button>
   <div class="ac-bb-modal">
     <div class="ac-bb-modal-background"></div>
     <div class="ac-bb-modal-content">
       <div class="ac-bb-box">
-        <iframe class="ac-iframe" src="http://localhost:5994/?product=kubedb&owner=system-admin" width="100%" ></iframe>
+        <iframe class="ac-iframe" src="http://deploy.bb.test:5994/?product=kubedb&owner=system-admin" width="100%" ></iframe>
       </div>
     </div>
     <button class="ac-bb-modal-close">X</button>
@@ -18,12 +17,16 @@ export function initialize(ref) {
 
   // catch all the elements
   const btn = document.querySelector('.ac-bb-modal-button');
+  const btnIcon = document.querySelector('.ac-bb-modal-button-icon');
   const acModal = document.querySelector('.ac-bb-modal');
   const acModalBg = document.querySelector('.ac-bb-modal-background');
   const acModalContent = document.querySelector('.ac-bb-modal-content');
   const acModalBox = document.querySelector('.ac-bb-box');
   const acIframe = document.querySelector('.ac-iframe');
-  acIframe.style.height = 'calc(100vh - 200px)';
+
+  // set modal height to almost 80% of the view port
+  const vh = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+  acIframe.style.height = `calc(100vh - ${vh * 0.185}px)`;
 
   const acModalClose = document.querySelector('.ac-bb-modal-close');
   // const acModalIframe = document.querySelector(".ac-iframe");
@@ -32,5 +35,5 @@ export function initialize(ref) {
   addEventListeners(btn, acModal, acModalBox, acModalClose);
 
   // add styles
-  addStyles(btn, acModal, acModalBg, acModalContent, acModalBox, acModalClose);
+  addStyles(btn, btnIcon, acModal, acModalBg, acModalContent, acModalBox, acModalClose);
 }
